9085a16cee54f2bd89be0d8aef94fe2d451e39fb
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2023-08-11 17:46:40 +0530

    fixed axios intercepter

--- src/utils/backendServerBaseURL.js
+++ src/utils/backendServerBaseURL.js
@@ -1,5 +1,4 @@
 let tempbackendServerBaseURL = process.env.REACT_APP_BACKEND_URL;
 console.log(process.env.REACT_APP_BACKEND_URL);
 
-export const backendServerBaseURL =
-	'https://f4f8-2406-7400-43-5d8b-293c-a6ad-72ee-a9b3.ngrok-free.app';
+export const backendServerBaseURL = tempbackendServerBaseURL;
