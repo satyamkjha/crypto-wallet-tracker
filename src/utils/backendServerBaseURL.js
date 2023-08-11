d706d258b2c3693e63634207dd2bd47e209e8b1f
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2023-08-11 17:49:04 +0530

    fixed axios intercepter

--- src/utils/backendServerBaseURL.js
+++ src/utils/backendServerBaseURL.js
@@ -1,9 +1,4 @@
 let tempbackendServerBaseURL = process.env.REACT_APP_BACKEND_URL;
 console.log(process.env.REACT_APP_BACKEND_URL);
 
-<<<<<<< HEAD
 export const backendServerBaseURL = tempbackendServerBaseURL;
-=======
-export const backendServerBaseURL =
-	'http://localhost:8000';
->>>>>>> origin/bug-fixes
